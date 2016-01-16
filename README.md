5e0ba3b75204dd5eb970d802a3ca5300e59380ba
ice201508 <2235907780@qq.com>
2016-01-16 18:30:29 +0800

    home add express

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 ### 学习不会的东西优先上官网看API
 
+[Express API](http://www.expressjs.com.cn/)
 [Jade官网](http://jade.tilab.com/)
 
 ### 开发环境介绍
@@ -28,6 +29,15 @@
 npm install express jade moment mongoose
 ```
 
+[serve-favicon 包说明](https://www.npmjs.com/package/serve-favicon)
+
++ 使用 app.use(express.favicon()); 会提示错误,中间件的问题 使用serve-favicon 代替
+
+```
+//文件名一定不能写错， .ico 不是 .icon
+app.use(favicon(__dirname+'/public/favicon.ico'));
+```
+
 + 代码中自己不知道的地方
 
 ```
@@ -37,3 +47,5 @@ var port=process.env.PORT || 3000;
 ```
 
 + jade 模板中 tab和空格只能使用一种,不能都使用
++ jade模板的优势  可以实现继承，可以提取公共部分。去调整目录结构，views里面修改
++ 各个页面的书写完成后，就是伪造数据测试，还没有涉及到数据库
--- app/app.js
+++ app/app.js
@@ -1,35 +1,65 @@
-var express=require('express');
-var port=process.env.PORT || 3000;
-var app=express();
+var express = require('express');
+var favicon = require('serve-favicon');
 
-app.set('views','./views');
-app.set('view engine','jade');
+var port = process.env.PORT || 3000;
+var app = express();
 
-app.listen(port,function(){
+app.set('views', './views');
+app.set('view engine', 'jade');
+app.use(favicon(__dirname+'/public/favicon.ico'));
+
+app.listen(port, function() {
 	console.log('servre is running on 3000');
 });
 
+var movies=[{
+	title:'机械战警',
+	_id:1,
+	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+},{
+	title:'机械战警',
+	_id:2,
+	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+},{
+	title:'机械战警',
+	_id:3,
+	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+},{
+	title:'机械战警',
+	_id:4,
+	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+},{
+	title:'机械战警',
+	_id:5,
+	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+},{
+	title:'机械战警',
+	_id:6,
+	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+}];
+
 // index page
-app.get('/',function(req,res){
-	res.render('index',{
-		title:'Film 首页'
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'Film 首页',
+		movies:movies
 	})
 });
 // detail page
-app.get('/movie/:id',function(req,res){
-	res.render('detail',{
-		title:'Film 详情页'
+app.get('/movie/:id', function(req, res) {
+	res.render('detail', {
+		title: 'Film 详情页'
 	})
 });
 // admin page
-app.get('/admin/movie',function(req,res){
-	res.render('admin',{
-		title:'Film 后台录入页'
+app.get('/admin/movie', function(req, res) {
+	res.render('admin', {
+		title: 'Film 后台录入页'
 	})
 });
 // list page
-app.get('/admin/list',function(req,res){
-	res.render('list',{
-		title:'Film 列表页'
+app.get('/admin/list', function(req, res) {
+	res.render('list', {
+		title: 'Film 列表页'
 	})
 });
