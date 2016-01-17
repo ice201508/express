2581f838a07e58ee365ef60d22617b5031b29206
ice201508 <2235907780@qq.com>
2016-01-17 14:22:15 +0800

    home add

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 
 [Express API](http://www.expressjs.com.cn/)
 [Jade官网](http://jade.tilab.com/)
+[Jade 引擎](http://jade-lang.com/)
 
 ### 开发环境介绍
 
@@ -46,6 +47,187 @@ var port=process.env.PORT || 3000;
 启动:  PORT=8000 node app.js
 ```
 
-+ jade 模板中 tab和空格只能使用一种,不能都使用
+### Jade规范
+
+[Jade API](http://naltatis.github.io/jade-syntax-docs/)
+
++ jade 模板中 缩进 tab和空格只能使用一种,不能都使用
 + jade模板的优势  可以实现继承，可以提取公共部分。去调整目录结构，views里面修改
-+ 各个页面的书写完成后，就是伪造数据测试，还没有涉及到数据库
++ 命令行编译 *.jade 为 *.html 文件
+
+```
+npm install jade -g
+//每次编译后都是自动压缩后的html文件
+// -P 表示不压缩
+// -w 表示开启监听模式,每次修改后自动编译
+jade -P -w jade.jade
+```
+
+> 各个页面的书写完成后，就是伪造数据测试，还没有涉及到数据库
+
+```
+res.render({
+	title:'index',
+	movies:[{title:'ss',id:1},{title:'ss',id:2}]
+});
+```
+
+#### id/class
+
+```
+//标签和文本必须要有缩进
+//元素的属性用()
+#content
+	.block
+		input#bar.foo1.foo2(type='text',required)
+```
+
+#### 文本
+
++ 标签和文本间要空格，()内写属性
++ 冒号的作用 :+空格 可以代替换行
++ 使用外部变量 '#{book.name}' h2= book.name
++ 变量可以用在属性内部
+
+```
+//文本书写的2种方式
+p.
+	foo bar
+	hello world
+
+p
+	| foo bar
+	| hello world
+//文本内添加标签
+p element in text <input type='text' required>
+//添加属性
+p
+	a.btn.btn-primary.del(href='/movie/#{id}' role='button') 播放
+//冒号代替换行
+ul#books
+	li: a(href='#book-a') book A
+	li: a(href='#book-b') book B
+//外部变量 2种方式的区别
+- var book={'name':'God Father','price':12.99}
+	h2= book.name
+	h3 '#{book.name}' is $#{book.price}
+//=会原样输出,!=会转义输出
+- var name='hello <em>World</em>'
+	li= name
+	li!= name
+//属性内部用变量
+input(type='text',placeholder='#{name}')
+
+- var user={id:12,name:'Lucy'}
+a(href='/user/'+user.id)= user.name
+```
+
+### 注释
+
++ 注释分为输出的和不输出的，单行的和块 // //-
++ 还支持条件注释
+
+```
+// begin animate
+//- begin animate
+缩进表示继承上一步的符号的含义
+// begin animate
+	sdf
+	asdf
+```
+
+#### if,unless,case
+
+```
+- var name='Lucy'
+	if name=='Lucy'
+		h1 this is lucy
+	else
+		h1!= name
+//unless 后面的条件为假时才执行后面的语句
+- var name=false
+	unless name
+		p no errors
+
+//case语句代替if/else
+case name
+	when 'bob'
+		p Hi bob
+	when 'Alice'
+		p hello alice
+	default
+		p hello #{name}
+```
+
+#### 循环 for/each 用来遍历数组和对象
+
+```
+//在jade中定义变量
+- var jobs=['beijing','shanghai','guangdong']
+		each job in jobs
+			li=job
+
+//也可以写
+ul
+	each job in jobs
+		li=job
+//each后面的参数可以有2个，一个表示序号,后面也可以有else语句
+- var books=['A','B','C']
+	select
+		each book,i in books
+			option(value=i) BOOK #{book}
+//for后面也可以有2个参数,并且还有有个else语句
+ul
+	for book,i in books
+		li(class='bar#{i}') #{book}
+	else
+		li no items
+```
+
+#### jade 3种可执行代码
+
+```
+- var foo = 'bar'
+
+- for(var key in obj)
+	p=obj[key]
+
+- if(foo)
+	ul
+		li yar
+- else
+	p on SF
+
+- if(items.lengths)
+	ul
+		- items.foreach(function(item){
+				li= item
+		- })
+```
+
+#### 模板继承 block/extends
+
++ block append head /  block prepend head / append head
+
+```
+//这里是引用外部的 .jade文件
+extends ../extend-layout
+//这里定义的块可以供外部使用
+block scrpits
+	script(src='/jquery.js')
+	script(src='/jquery.js')
+block content
+	h1= title
+```
+
++ 包含 include/yield 静态文件
+
+```
+html
+	include includes/head
+body
+	p text
+	include includes/foot
+```
+
+#### shell 中的makefile文件
--- app/app.js
+++ app/app.js
@@ -1,65 +1,101 @@
 var express = require('express');
+var path = require('path');
+var bodyParser=require('body-parser');
 var favicon = require('serve-favicon');
 
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.set('views', './views');
+app.set('views', './views/pages');
 app.set('view engine', 'jade');
-app.use(favicon(__dirname+'/public/favicon.ico'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.static(path.join(__dirname,'bower_components')));
 
 app.listen(port, function() {
 	console.log('servre is running on 3000');
 });
 
-var movies=[{
-	title:'机械战警',
-	_id:1,
-	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-},{
-	title:'机械战警',
-	_id:2,
-	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-},{
-	title:'机械战警',
-	_id:3,
-	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-},{
-	title:'机械战警',
-	_id:4,
-	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-},{
-	title:'机械战警',
-	_id:5,
-	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-},{
-	title:'机械战警',
-	_id:6,
-	poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+var movies = [{
+	title: '机械战警',
+	_id: 1,
+	poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+}, {
+	title: '机械战警',
+	_id: 2,
+	poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+}, {
+	title: '机械战警',
+	_id: 3,
+	poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+}, {
+	title: '机械战警',
+	_id: 4,
+	poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+}, {
+	title: '机械战警',
+	_id: 5,
+	poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+}, {
+	title: '机械战警',
+	_id: 6,
+	poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
 }];
 
 // index page
 app.get('/', function(req, res) {
 	res.render('index', {
 		title: 'Film 首页',
-		movies:movies
+		movies: movies
 	})
 });
 // detail page
 app.get('/movie/:id', function(req, res) {
 	res.render('detail', {
-		title: 'Film 详情页'
+		title: 'Film 详情页',
+		movie: {
+			doctor: '迈克尔.乔丹',
+			country: '美国',
+			title: '机械战警',
+			year: 2014,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+			language: '英语',
+			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			submary: '《教父》是派拉蒙影业公司出品的一部黑帮题材的电影，改编自马里奥·普佐的同名小说，由弗朗西斯·福特·科波拉执导，马龙·白兰度、阿尔·帕西诺等主演。影片于1972年在美国上映。该片讲述了以维托·唐·科莱昂为首的黑帮家族的发展过程以及科莱昂的小儿子迈克如何接任父亲成为黑帮首领的故事。1973年这部电影获得了第45届奥斯卡奖最佳电影、最佳男主角、最佳改编剧本三项奖项；此外还获得了第30届美国金球奖最佳电影奖、最佳导演等奖项。2007年美国电影协会评选了“百年百佳影片”，《教父》排名第二。'
+		}
 	})
 });
 // admin page
 app.get('/admin/movie', function(req, res) {
 	res.render('admin', {
-		title: 'Film 后台录入页'
+		title: 'Film 后台录入页',
+		movie: {
+			title: '',
+			doctor: '',
+			country: '',
+			year: '',
+			poster: '',
+			flash: '',
+			summary: '',
+			language: ''
+		}
 	})
 });
 // list page
 app.get('/admin/list', function(req, res) {
 	res.render('list', {
-		title: 'Film 列表页'
+		title: 'Film 列表页',
+		movies: [{
+			title: '机械战警',
+			_id: 1,
+			doctor: '迈克尔.乔丹',
+			country: '美国',
+			year: 2014,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+			language: '英语',
+			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			submary: '《教父》是派拉蒙影业公司出品的一部黑帮题材的电影，改编自马里奥·普佐的同名小说，由弗朗西斯·福特·科波拉执导，马龙·白兰度、阿尔·帕西诺等主演。影片于1972年在美国上映。该片讲述了以维托·唐·科莱昂为首的黑帮家族的发展过程以及科莱昂的小儿子迈克如何接任父亲成为黑帮首领的故事。1973年这部电影获得了第45届奥斯卡奖最佳电影、最佳男主角、最佳改编剧本三项奖项；此外还获得了第30届美国金球奖最佳电影奖、最佳导演等奖项。2007年美国电影协会评选了“百年百佳影片”，《教父》排名第二。'
+		}]
 	})
 });
